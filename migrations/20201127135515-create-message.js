64bd8dc4ee1fb47e005c3f2b3989651bfc85b155
RDOToole89 <roibinotoole@gmail.com>
2020-11-27 14:59:43 +0100

    Create message model, migrations setup relations

---
+++ migrations/20201127135515-create-message.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("messages", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.TEXT,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      recipientUserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("messages");
+  },
+};
---
+++ models/message.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      message.belongsTo(models.user);
+    }
+  }
+  message.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.TEXT,
+      userId: DataTypes.INTEGER,
+      recipientUserId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "message",
+    }
+  );
+  return message;
+};
